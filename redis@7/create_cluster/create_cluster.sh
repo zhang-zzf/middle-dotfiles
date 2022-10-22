59f1a0e8057c0b8b54d282a0c1f797f3cf08bc3b
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-10-22 12:25:42 +0800

    feat: fix bugs

--- redis@7/create_cluster/create_cluster.sh
+++ redis@7/create_cluster/create_cluster.sh
@@ -10,27 +10,27 @@ rm -f redis_nodes.txt
 # admin@10.255.1.1:22 7000 16
 _create_node() {
   cd ${workdir}
-  address=${1}
-  redisPort=${2}
-  num=${3}
+  local address=${1}
+  local redisPort=${2}
+  local num=${3}
   # username@ip:port
   # 注意 有个空格
-  arr=(${address//@/ })
+  local arr=(${address//@/ })
   if [ ${#arr[*]} == 1 ]; then
     # 10.255.1.1:22
-    username="admin"
+    local username="admin"
     arr=(${arr[0]//:/ })
   else
     # admin@10.255.1.1:22
-    username=${arr[0]}
+    local username=${arr[0]}
     arr=(${arr[1]//:/ })
   fi
-  nodeIp=${arr[0]}
+  local nodeIp=${arr[0]}
   # default 22
-  nodePort=${arr[1]:-22}
+  local nodePort=${arr[1]:-22}
   # scp bin to remote node
   cd ${workdir}
-  scp -P ${nodePort} -r redis_7.0.5_64 ${username}@${nodeIp}:
+  scp -P ${nodePort} -r redis_7.0.5_64 ${username}@${nodeIp}: &>/dev/null
   ssh -p ${nodePort} ${username}@${nodeIp} "pkill -9 redis-server && sleep 5s; \
       cd ~; \
       rm -r redis_cluster; mkdir -p redis_cluster; \
@@ -43,10 +43,11 @@ _create_node() {
   for i in $(seq 0 ${num}); do
     # 数字加法
     ((port = ${redisPort} + ${i}))
+    echo "redis instance-> ${address}, ${port}"
     mkdir ${port}
     cp ${workdir}/redis.conf ${port}
     sed -i "1s/6379/${port}/" ${port}/redis.conf
-    scp -r ${port} ${username}@${nodeIp}:~/redis_cluster/
+    scp -r ${port} ${username}@${nodeIp}:~/redis_cluster/ &>/dev/null
     ssh ${username}@${nodeIp} "cd ~/redis_cluster/${port}; \
           ~/redis_cluster/bin/redis-server redis.conf \
           "
@@ -57,6 +58,7 @@ _create_node() {
 
 _interactive() {
   echo "" >${workdir}/redis_nodes.txt
+  instances=""
   while :; do
     cd ${workdir}
     echo "dirname: ${workdir}"
@@ -68,7 +70,7 @@ _interactive() {
     num=${num:-4}
     read -p "Enter the start port[default 7000]: " redisPort
     redisPort=${redisPort:-7000}
-    instances="${instances} $(_create_node ${nodeAddress} ${redisPort} ${num})"
+    _create_node ${nodeAddress} ${redisPort} ${num}
   done
   echo "Redis instances: ${instances}"
   read -p "Now create redis_cluster, replicas[default 1]: " replicas
@@ -84,10 +86,11 @@ _batch() {
   # 注意空格
   addresses=(${1//,/ })
   redisStartPort=${2}
-  num=${3}
+  redisInstancePerNode=${3}
   replicas=${4:-1}
+  instances=""
   for nodeAddress in ${addresses[@]}; do
-    instances="${instances} $(_create_node ${nodeAddress} ${redisStartPort} ${num})"
+    _create_node ${nodeAddress} ${redisStartPort} ${redisInstancePerNode}
   done
   echo "Redis instances: ${instances}"
   ${workdir}/redis_7.0.5_64/redis-cli --cluster create ${instances} --cluster-replicas 1
@@ -98,11 +101,8 @@ batch)
   shift
   _batch $@
   ;;
-interactive)
-  shift
-  _interactive
-  ;;
 *)
   echo "Usage: ${0} batch admin@10.255.1.41:22,admin@10.255.1.42 7000 16 1"
+  _interactive
   ;;
 esac
